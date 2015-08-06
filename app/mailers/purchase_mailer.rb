06a1f17f64d0613abfe8ab2e894f69c9b69f837b
cmkoller <cmkoller01@gmail.com>
2015-08-06 18:49:57 -0400

    Send games as email

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,6 +1,11 @@
+require 'open-uri'
+
 class PurchaseMailer < ApplicationMailer
   def new_purchase(game_ids, email)
-    @game_ids = game_ids
+    @games = Game.find(game_ids)
+    @games.each do |game|
+      attachments["#{game.title}"] = open("#{game.file.url}").read
+    end
     @email = email
     mail(
       to: @email,
