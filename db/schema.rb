5c8d9556b2f59f6d9dd88061cc968ed6ce4d6a59
cmkoller <cmkoller01@gmail.com>
2015-08-06 19:03:35 -0400

    Add description to games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806225420) do
+ActiveRecord::Schema.define(version: 20150806230155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150806225420) do
     t.float  "price"
     t.string "file"
     t.string "featured_image"
+    t.text   "description"
   end
 
   create_table "purchases", force: :cascade do |t|
