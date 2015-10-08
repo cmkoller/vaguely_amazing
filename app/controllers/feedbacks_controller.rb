69a7c323ce59b525191aa4a4ee5e3188d7861553
cmkoller <cmkoller01@gmail.com>
2015-10-07 22:43:41 -0400

    Allow all feedback form, name not required

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -13,6 +13,40 @@ class FeedbacksController < ApplicationController
   protected
 
   def feedback_params
-    params.require(:feedback).permit(:name, :email, :feedback)
+    params.require(:feedback).permit(
+      :name, :email, :other_feedback,
+
+      :encounters_frustrating,
+      :encounters_fun,
+      :encounters_simple,
+      :encounters_complex,
+      :encounters_logical,
+      :encounters_appealing,
+      :encounters_unappealing,
+
+      :items_frustrating,
+      :items_fun,
+      :items_simple,
+      :items_complex,
+      :items_logical,
+      :items_appealing,
+      :items_unappealing,
+
+      :classes_frustrating,
+      :classes_fun,
+      :classes_simple,
+      :classes_complex,
+      :classes_logical,
+      :classes_appealing,
+      :classes_unappealing,
+      
+      :cooperative_frustrating,
+      :cooperative_fun,
+      :cooperative_simple,
+      :cooperative_complex,
+      :cooperative_logical,
+      :cooperative_appealing,
+      :cooperative_unappealing
+    )
   end
 end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,2 @@
 class Feedback < ActiveRecord::Base
-  validates :name, presence: true
 end
